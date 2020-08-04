1a02397c77e5a5f77a9d389062912d0f8727a397
Richard B.RiX <44611385+ZapRosen@users.noreply.github.com>
2020-08-04 13:59:13 +0200

    Add files via upload

---
+++ Java/Variablen/VariablenAusgabe.java
@@ -0,0 +1,27 @@
+package com.example.variablenausgabe;
+
+public class VariablenAusgabe {
+    public static void main(String[] args){
+        int meiNumber = 33;
+        short meiShort =333;
+        long meiLong =4444;
+
+        double meiDouble = 5.334;
+        float meiFloat = 300.3f;
+
+        char meiChar = 'y';
+        boolean meiBoolean = false;
+
+        byte meiByte = 127;
+
+        System.out.println(meiNumber);
+        System.out.println(meiShort);
+        System.out.println(meiLong);
+        System.out.println(meiDouble);
+        System.out.println(meiFloat);
+        System.out.println(meiChar);
+        System.out.println(meiBoolean);
+        System.out.println(meiByte);
+
+    }
+}
