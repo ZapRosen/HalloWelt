64a3bd00a1e56e5d19eca88cc0525e439aec3de9
Richard B.RiX <44611385+ZapRosen@users.noreply.github.com>
2020-08-17 22:21:56 +0200

    Add files via upload

---
+++ Java/HalloObjekt/Spielplatz/Spieler.java
@@ -0,0 +1,17 @@
+/* In diesem Beispiel 2 wird die Aussage
++  an eine Variable übergeben.
+ */
+public class Spieler {
+    String sagHallo(){
+    var strgvar = "Ich bin der Spruch als Variable strgvar";
+        return strgvar;
+
+
+    }
+    String sagWas(){
+        var strgvar2 = "Cool hier! strgvar2";
+        return strgvar2;
+
+
+    }
+}
---
+++ Java/HalloObjekt/Spielplatz/Spielplatz.java
@@ -0,0 +1,7 @@
+public class Spielplatz {
+    public static void main(String[] args) {
+        Spieler frank = new Spieler();
+        System.out.println(frank.sagHallo());
+        System.out.println(frank.sagWas());
+    }
+}
