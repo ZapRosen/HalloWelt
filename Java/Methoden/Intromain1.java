78aa9861b5700c265f337da893aa356223e2a8f0
Richard B.RiX <44611385+ZapRosen@users.noreply.github.com>
2020-09-01 13:38:43 +0200

    Update Intromain1.java

--- Java/Methoden/Intromain1.java
+++ Java/Methoden/Intromain1.java
@@ -1,20 +1,44 @@
-//Kapitel 9 Kofler, Java Projekt Methode-intro1
-public class Intromain1 {
+/*Kofler Seite183 ff
+* Methoden m1 bis m...
+ */
+
+
+import java.awt.Point;
+public class Kap9Point2 {
+
+    // Startpunkt des Programms
     public static void main(String[] args) {
-        System.out.println("Jetzt:In der Mainmethode");
-        Methode1();
-        System.out.println("Nach Aufruf M1 wieder main");
+        System.out.println("In main()");
+        m1();   // Aufruf der Methode m1
+        System.out.println("Wieder in main()");
     }
-    public static void Methode1() {
-        System.out.println("In  Methode1()");
-        int ergebnis = Methode2(7,12);
-        System.out.println("Ergebnis: "+ergebnis);
+
+    // Methode m1
+    public static void m1() {
+        String s ="abc";
+        String t ="xyz"; /*Der String t hat den
+        *Anfangswert von xyz. In m4 wird das Attribut irgendwas übergeben.
+*/
+        m2(s); //Aufruf Methode m2
+        System.out.println(s); //Ausgabe abc
+        m3(0); //Aufruf Methode m3
+        System.out.println(m3(0));
+        m4(t); //Aufruf Methode m4
+        System.out.println(m4(t));
+
+      }
+
+    // Methode m2
+    public static void m2(String p) {
+        p = p+"x"; // p+"x" wird nicht zurückgegeben.
+       }
+    public static int m3(int p){
+        p=4;
+        return p;
     }
-    public static int Methode2(int a, int b) {
-        System.out.println("In Methode2()");
-        if(a>b)
-            return a*b;
-        else
-            return a+2*b;
+    public static String m4(String t){
+        t="irgendwas"; //Es wird der String "irgendwas" zurücckgegeben.
+        return t;
+
     }
 }
